4e965fdf538ffd7235eb4d18d55ce4b71b7ba91e
qmotas <1974745+qmotas@users.noreply.github.com>
2023-01-20 16:23:09 +0900

    update packages

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>cn-msa-research-note</span>,
+  project: {
+    link: 'https://github.com/qmotas/cn-msa-research-note',
+  },
+  docsRepositoryBase: 'https://github.com/qmotas/cn-msa-research-note',
+  feedback: {
+    content: null,
+  },
+  footer: {
+    text: 'cn-msa-research-note',
+  },
+}
+
+export default config
